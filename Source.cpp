9c502629ade50f5353b052a34cd39cda42cc781f
bumblebasketball <81335884+bumblebasketball@users.noreply.github.com>
2021-03-24 19:59:11 -0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -34,22 +34,8 @@ void getRaceTimes(string& racerName, double& raceTime)
 {
 	cout << "Please enter the racer's first name > ";
 	cin >> racerName;
-	cout << "Please enter the racer's time > ";
-	do
-	{
-		if (raceTime >= 0)
-		{
-			cin >> raceTime;
-		}
-
-		else
-		{
-			cout << "Invalid" << endl;
-		}
-
-	} while (raceTime >= 0);
-
-
+	cout << "Please enter the racer's time > "
+	cin >> raceTime;
 }
 
 void findWinner(string racer1, string racer2, string racer3, double time1, double time2, double time3)
